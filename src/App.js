3a5594568c826b0d25ab4cbc5cc475a1855671bb
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    fixed the merge issue, kept corrected logo and name

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Reid Castillo and this is my project </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={gatorLogo} className="App-logo" alt="logo" />
+                <p> Hello World! </p>
+                <p> My name is Reid Castillo and this is my project </p>
+                <p> Hello World! This is our team project. </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
